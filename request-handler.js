ed3f1b255bb3d7c63dbdc18ba00e7f0a8a6b997c
Hack Reactor Students <github@hackreactor.com>
2013-08-26 20:05:23 -0700

    added 404s

--- request-handler.js
+++ request-handler.js
@@ -1,44 +1,60 @@
-/* You should implement your request handler function in this file.
- * But you need to pass the function to http.createServer() in
- * basic-server.js.  So you must figure out how to export the function
- * from this file and include it in basic-server.js. Check out the
- * node module documentation at http://nodejs.org/api/modules.html. */
 var storage = require('./storage.js').storage;
+var url = require('url');
+var http = require("http");
+
 var responseHeaders = {
     'Content-Type': 'text/plain',
     "access-control-allow-origin": "*",
     "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
     "access-control-allow-headers": "content-type, accept",
     "access-control-max-age": 10 // Seconds.
-  };
+};
 
-exports.handleRequest = function(request, response) {
-  var body = 'Hello, Brainwave';
+exports.requestRouter = function (request, response) {
+  var urlObj = url.parse(request.url);
 
-  response.writeHead(200, responseHeaders);
+  if (urlObj.path === '/1/classes/messages') {
+    if (request.method === 'OPTIONS') {
+      response.writeHead(200, {
+        'Content-Type': 'text/plain',
+        "access-control-allow-origin": "*",
+        "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "access-control-allow-headers": "content-type, accept",
+        "access-control-max-age": 10 // Seconds.
+      });
 
-  response.write(body);
+      response.end();
+    }
 
-  response.end();
+    if (request.method === 'POST') {
+      exports.messageHandler(request, response);
+    }
+
+    if (request.method === 'GET') {
+      exports.sendMessageHandler(request, response);
+    }
+  } else {
+    response.writeHead(404);
+    response.end();
+  }
 };
 
 exports.messageHandler = function(request, response) {
-  var body = 'response works';
-
-  response.writeHead(200, responseHeaders);
+  response.writeHead(201, responseHeaders);
 
   request.on('data', function(data) {
     var stringData = data.toString();
     storage.results.push(JSON.parse(stringData));
-    console.log(storage.results);
   });
 
-  response.end();
+  request.on('end', function() {
+    response.end();
+  });
 };
 
 exports.sendMessageHandler = function(request, response) {
   responseHeaders['Content-Type'] = 'application/json';
   response.writeHead(200, responseHeaders);
-  response.write(JSON.stringify(storage));
+  response.write(JSON.stringify(storage.results));
   response.end();
 };
