6a35484fedff065e8ca9c9c2ef1e21553a803230
Hack Reactor Students <github@hackreactor.com>
2013-08-26 17:40:58 -0700

    handling get requests now.

--- request-handler.js
+++ request-handler.js
@@ -4,17 +4,18 @@
  * from this file and include it in basic-server.js. Check out the
  * node module documentation at http://nodejs.org/api/modules.html. */
 var storage = require('./storage.js').storage;
-
-exports.handleRequest = function(request, response) {
-  var body = 'Hello, Brainwave';
-
-  response.writeHead(200, {
+var responseHeaders = {
     'Content-Type': 'text/plain',
     "access-control-allow-origin": "*",
     "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
     "access-control-allow-headers": "content-type, accept",
     "access-control-max-age": 10 // Seconds.
-  });
+  };
+
+exports.handleRequest = function(request, response) {
+  var body = 'Hello, Brainwave';
+
+  response.writeHead(200, responseHeaders);
 
   response.write(body);
 
@@ -24,14 +25,7 @@ exports.handleRequest = function(request, response) {
 exports.messageHandler = function(request, response) {
   var body = 'response works';
 
-  response.writeHead(200, {
-    'Content-Length': body.length,
-    'Content-Type': 'text/plain',
-    "access-control-allow-origin": "*",
-    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "access-control-allow-headers": "content-type, accept",
-    "access-control-max-age": 10 // Seconds.
-  });
+  response.writeHead(200, responseHeaders);
 
   request.on('data', function(data) {
     var stringData = data.toString();
@@ -39,5 +33,12 @@ exports.messageHandler = function(request, response) {
     console.log(storage.results);
   });
 
+  response.end();
+};
+
+exports.sendMessageHandler = function(request, response) {
+  responseHeaders['Content-Type'] = 'application/json';
+  response.writeHead(200, responseHeaders);
+  response.write(JSON.stringify(storage));
   response.end();
 };
