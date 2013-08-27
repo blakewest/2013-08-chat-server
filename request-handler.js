f569ca87645f0d5a47177d4c7efb1eb28577a8e5
Hack Reactor Students <github@hackreactor.com>
2013-08-26 17:19:06 -0700

    add handler functions for post request and options preflight

--- request-handler.js
+++ request-handler.js
@@ -3,7 +3,41 @@
  * basic-server.js.  So you must figure out how to export the function
  * from this file and include it in basic-server.js. Check out the
  * node module documentation at http://nodejs.org/api/modules.html. */
+var storage = require('./storage.js').storage;
 
-var handleRequest = function(request, response) {
+exports.handleRequest = function(request, response) {
+  var body = 'Hello, Brainwave';
 
+  response.writeHead(200, {
+    'Content-Type': 'text/plain',
+    "access-control-allow-origin": "*",
+    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "access-control-allow-headers": "content-type, accept",
+    "access-control-max-age": 10 // Seconds.
+  });
+
+  response.write(body);
+
+  response.end();
+};
+
+exports.messageHandler = function(request, response) {
+  var body = 'response works';
+
+  response.writeHead(200, {
+    'Content-Length': body.length,
+    'Content-Type': 'text/plain',
+    "access-control-allow-origin": "*",
+    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "access-control-allow-headers": "content-type, accept",
+    "access-control-max-age": 10 // Seconds.
+  });
+
+  request.on('data', function(data) {
+    var stringData = data.toString();
+    storage.results.push(JSON.parse(stringData));
+    console.log(storage.results);
+  });
+
+  response.end();
 };
